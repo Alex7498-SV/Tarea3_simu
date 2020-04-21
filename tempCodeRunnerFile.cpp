a577b1fdaf4b0b6439d9c6a05d41fb4fd402806c
Alex7498-SV <00068817@uca.edu.sv>
2020-04-21 16:38:15 -0600

    finalizado

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+
+    cout << "IMPLEMENTACI"<<char(224)<<"N DEL M"<<char(144)<<"TODO DE LOS ELEMENTOS FINITOS\n"
+         << "\t- TRANSFERENCIA DE CALOR\n" << "\t- 1 DIMENSI"<<char(224)<<"N\n"
+         << "\t- FUNCIONES DE FORMA LINEALES\n" << "\t- PESOS DE GALERKIN\n"
+         << "*********************************************************************************\n\n";
